3b5e3f55e8eb2805f7f050ed2770429ef7fd32a4
Lumi Valkyrie <Lumi.Valkyrie.Ipema@gmail.com>
2020-12-14 20:05:11 +0100

    wtfff

---
+++ Script.js
@@ -0,0 +1,9 @@
+var answers = {"Lust":0,"Gluttony":0,"Greed":0,"Sloth":0,"Wrath":0,"Envy":0,"Pride":0}
+for (key in answers){
+    var button=document.createElement("button");
+    button.innerText=key;
+    button.setAttribute("onclick",`doFunction("${key}")`);
+    button.appendChild(button);
+};
+
+Function doFunction
